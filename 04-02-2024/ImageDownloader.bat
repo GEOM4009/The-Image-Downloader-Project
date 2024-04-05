07fa294a7d7d63c3632da1b754c2159d6877f243
ZSauve <zachsauve1@gmail.com>
2024-04-05 10:15:59 -0400

    cleaning up

--- 04-02-2024/ImageDownloader.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:start
-"C:\Users\zachs\anaconda3\envs\geom4009\python.exe" "C:\Users\zachs\Documents\GitHub\The-Image-Downloader-Project\04-02-2024\ImageDownloaderProject.py" "C:\Users\zachs\Documents\GitHub\The-Image-Downloader-Project\04-02-2024\config.cfg"
-timeout /t 300 /nobreak >nul
-goto start
