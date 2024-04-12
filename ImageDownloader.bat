970e009ea9ed3100eb82cbbebda22ad3ee7ea1ed
ZSauve <zachsauve1@gmail.com>
2024-04-12 13:55:54 -0400

    Add Power Point

--- ImageDownloader.bat
+++ ImageDownloader.bat
@@ -2,4 +2,4 @@
 :start
 "C:\Users\zachs\anaconda3\envs\imagedownloader\python.exe" "C:\Users\zachs\Documents\GitHub\The-Image-Downloader-Project\ImageDownloaderProject.py" "C:\Users\zachs\Documents\GitHub\The-Image-Downloader-Project\config.cfg"
 timeout /t 300 /nobreak >nul
-goto start
+goto starts
